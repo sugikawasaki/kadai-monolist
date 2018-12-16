77577ab3661f3c9b81efc80137375875bc2ab520
sugi.kawasaki <snakano@u-go.co.jp>
2018-12-16 07:58:39 +0000

    want

--- app/Item.php
+++ app/Item.php
@@ -4,7 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class item extends Model
+class Item extends Model
 {
-    //
+    protected $fillable = ['code', 'name', 'url', 'image_url'];
+    
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withPivot('type')->withTimestamps();
+    }
+    
+    public function want_users()
+    {
+        return $this->users()->where('type', 'want');
+    }
 }
